9b7f4f28c926aa9e8c20b9f898a5ec9f951fbcbd
Harsha Suraweera <harshamanoj912@gmail.com>
2024-01-21 13:28:44 +0530

    env variables

--- playwright.config.js
+++ playwright.config.js
@@ -7,7 +7,9 @@ module.exports = defineConfig({
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 1 : 0,
     workers: process.env.CI ? 2 : undefined,
-    reporter: 'html',
+    reporter: [
+        ['html', {open: "never"}]
+    ],
     timeout: 190000, //increased due to the queued jobs
 
     use: {
